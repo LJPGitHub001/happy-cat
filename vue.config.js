5952ef8f764043229718ec313d6b875986e3b681
LJP <ljp@LJPdeMacBook-Pro.local>
2021-06-05 18:56:56 +0800

    two

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/upload/website/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '.git/' : '/',
   devServer:{
     host: '0.0.0.0',
   }
